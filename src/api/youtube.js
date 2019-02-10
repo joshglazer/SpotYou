f700d6f469f1e387e9c668725e0e0e7558166f98
Josh Glazer <joshglazer@gmail.com>
2019-02-10 00:07:51 -0500

    more fun with environment variables

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,4 +1,4 @@
-const key = process.env.YOUTUBE_API_KEY;
+const key = process.env.GATSBY_YOUTUBE_API_KEY;
 
 export async function search(searchTerm) {
   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=${key}&q=${searchTerm}`;
