deee91a38a9bd1caa0699af3d8c7957aa3ea1678
Josh Glazer <joshglazer@gmail.com>
2019-02-13 08:10:40 -0500

    removing debug code

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,6 @@ export default class IndexPage extends Component {
 
   async componentDidMount() {
     ReactGA.initialize(process.env.GATSBY_GA_UA_ID);
-    console.log(`Initialize Google Analytics - ${process.env.GATSBY_GA_UA_ID}`)
     // handle Spotify authroization flow
     const parsedHash = queryString.parse(this.props.location.hash);
     if (parsedHash['access_token']) {
